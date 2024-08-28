d49f2a433b4129d4e969d5ee3d5d0ad2f4c99c95
vky25 <93203895+vky25@users.noreply.github.com>
2024-08-28 15:14:39 +0530

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Build script
+set -eo pipefail
+
+build_tag=$1
+name=nulpchatbot
+node=$3
+org=$4
+
+# Define image name and tag
+imageName="${name}"
+imageTag="${build_tag}"
+
+# Build Docker image
+docker build -t "${org}/${imageName}:${imageTag}" .
+
+# Create metadata.json with the relevant details
+echo "{\"image_name\" : \"${imageName}\", \"image_tag\" : \"${imageTag}\", \"node_name\" : \"${node}\"}" > metadata.json
